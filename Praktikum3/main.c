f7917231d99da91e8e26d8e44879afdb36f2f8b2
TimoDragon <toothless@nightfury.dev>
2025-11-26 18:13:27 +0100

    Praktikum 3

--- Praktikum3/main.c
+++ Praktikum3/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
+int split(char *string, char splitChar, char **parts, int maxParts, int maxLength);
+
 int main(void) {
-    printf("Hello, World!\n");
+    char partsbuf[10][200];
+    char *parts[10];
+
+    for (int i = 0; i < 10; i++) {
+        parts[i] = partsbuf[i];
+    }
+
+    int k = split("Blau;Gelb;Rot", ';', parts, 5, 200);
+
+    printf("%d\n", k);
+
+    for (int i = 0; i < k; i++) {
+        printf("%s\n", parts[i]);
+    }
+
     return 0;
+}
+
+int split(char *string, char splitChar, char **parts, int maxParts, int maxLength) {
+    int counter = 0;
+
+    // i = index von string, j = position von einem char in einem part
+    for (int i = 0, j = 0; i < maxLength && counter < maxParts; i++) {
+        if (string[i] == '\0') {
+            parts[counter][j] = '\0';
+            break;
+        }
+
+        if (string[i] == splitChar) {
+            parts[counter][j] = '\0';
+            counter += 1;
+            j = 0;
+            continue;
+        }
+
+        parts[counter][j] = string[i];
+        j++;
+    }
+
+    return counter+1;
 }
